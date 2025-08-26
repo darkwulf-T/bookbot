18c6d02600fa4c60a6604eef4a180bffac7ba749
darkwulf <tech.darkwulf@gmail.com>
2025-08-26 18:40:28 +0200

    finished boockbot project

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def get_book_text(filepath):
-    with open(filepath) as f:
+    with open(filepath, encoding="utf-8", errors="ignore") as f:
         file_contents = f.read()
     return file_contents
 
 def main(filepath):
     print(get_book_text(filepath))
 
-#from stats import count_words
+from stats import count_words
 
 #print(f"{count_words(get_book_text("books/frankenstein.txt"))} words found in the document")
 
 from stats import count_characters
 
-get_character = count_characters(get_book_text("books/frankenstein.txt"))
+#get_character = count_characters(get_book_text("books/frankenstein.txt"))
 
-print(get_character)
+#print(get_character)
+
+from stats import sort_function
+
+final_list = sort_function(count_characters(get_book_text(sys.argv[1])))
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {sys.argv[1]}...")
+print("----------- Word Count ----------")
+print(f"Found {count_words(get_book_text(sys.argv[1]))} total words")
+print("--------- Character Count -------")
+for char in final_list:
+    print(f"{char['char']}: {char['num']}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def count_characters(file_string):
         current = character_count.get(character,0)
         character_count[character] = current + 1
     return character_count
+
+def sort_on(items):
+    return items["num"]
+
+def sort_function(character_dic):
+    sorted_list = []
+    for char, num in character_dic.items():
+        if char.isalpha():
+            new_char_dict = {"char": char, "num": num}
+            sorted_list.append(new_char_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
